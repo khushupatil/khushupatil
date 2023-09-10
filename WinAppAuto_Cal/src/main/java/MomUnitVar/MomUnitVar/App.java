2b5659ff281598781f5598b2f26f89ffe49f42f1
WQ655SD <WQ655SD@IN3448516W1.ey.net>
2023-09-10 15:19:51 +0530

    first commit

---
+++ WinAppAuto_Cal/src/main/java/MomUnitVar/MomUnitVar/App.java
@@ -0,0 +1,13 @@
+package MomUnitVar.MomUnitVar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
